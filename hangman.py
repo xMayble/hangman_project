b8da0decb6923812084837f5a4440ce9183b37e2
Mayble <mehburohit@gmail.com>
2023-10-18 00:45:32 -0400

    check if user input matches

--- hangman.py
+++ hangman.py
@@ -4,3 +4,10 @@ word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
+
+guess = input("Guess a letter!\n").lower()
+for char in chosen_word:
+    if guess == char:
+        print("Right")
+    else:
+        print("Wrong")
