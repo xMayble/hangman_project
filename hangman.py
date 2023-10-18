d302a41ede726241014e5f1a838adeb4a8c80769
Mayble <mehburohit@gmail.com>
2023-10-18 00:54:01 -0400

    replaced blanks with guesses

--- hangman.py
+++ hangman.py
@@ -5,9 +5,14 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
+display = []
+for letter in chosen_word:
+    display.append("_")
+print(display)
+
 guess = input("Guess a letter!\n").lower()
-for char in chosen_word:
-    if guess == char:
-        print("Right")
-    else:
-        print("Wrong")
+
+for i in range(len(chosen_word)):
+    if guess == chosen_word[i]:
+        display[i] = guess
+print(display)
