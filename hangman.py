3cbddffa01ea6dacd77badc1ec815710d117491e
Mayble <mehburohit@gmail.com>
2023-10-18 22:48:52 -0400

    checking if player wins

--- hangman.py
+++ hangman.py
@@ -1,18 +1,27 @@
 import random
 
+# collection of words
 word_list = ["aardvark", "baboon", "camel"]
 
+# randomly chosen word from collection
 chosen_word = random.choice(word_list)
-print(chosen_word)
 
+# variable to show to user as they try to uncover the word
 display = []
+
+# iterate through the random word and add a blank for every character
 for letter in chosen_word:
     display.append("_")
-print(display)
 
-guess = input("Guess a letter!\n").lower()
+end_of_game = False
 
-for i in range(len(chosen_word)):
-    if guess == chosen_word[i]:
-        display[i] = guess
-print(display)
+while not end_of_game:
+    guess = input("Guess a letter!\n").lower()
+    for i in range(len(chosen_word)):
+        if guess == chosen_word[i]:
+            display[i] = guess
+    print(display)
+    if "_" not in display:
+        end_of_game = True 
+        print("You Win!")
+
