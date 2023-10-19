615deb40d420e3ddd386b844355b3afe0855f6ac
Mayble <mehburohit@gmail.com>
2023-10-19 01:01:30 -0400

    keeping track of player's life

--- hangman.py
+++ hangman.py
@@ -1,5 +1,63 @@
 import random
 
+# hangman strings
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 # collection of words
 word_list = ["aardvark", "baboon", "camel"]
 
@@ -14,14 +72,22 @@ for letter in chosen_word:
     display.append("_")
 
 end_of_game = False
-
+lives = 6
 while not end_of_game:
     guess = input("Guess a letter!\n").lower()
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
+    
     print(display)
+    if guess not in chosen_word:
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            print("You Lost!")
+            end_of_game = True
     if "_" not in display:
         end_of_game = True 
         print("You Win!")
+       
 
