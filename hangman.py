74edadd37c38b65da70cdb9ecdde8404206bfdb8
Mayble <mehburohit@gmail.com>
2023-10-19 01:43:11 -0400

    improv user exp

--- hangman.py
+++ hangman.py
@@ -1,68 +1,9 @@
 import random
-
-# hangman strings
-stages = [r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', r'''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', r'''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-# collection of words
-word_list = ["aardvark", "baboon", "camel"]
+import hangman_words as words
+import hangman_art as art
 
 # randomly chosen word from collection
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(words.word_list)
 
 # variable to show to user as they try to uncover the word
 display = []
@@ -73,19 +14,26 @@ for letter in chosen_word:
 
 end_of_game = False
 lives = 6
+
+print(art.logo)
+print(f"Pssst, the solution is {chosen_word}")
 while not end_of_game:
-    guess = input("Guess a letter!\n").lower()
+    guess = input("Guess a letter:" + " ").lower()
+    for char in display:
+        if char == guess:
+            print(f"You already guessed {guess}")
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
-    
-    print(display)
+    print(display)    
     if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life...")
+        print("----------------------")
         lives -= 1
-        print(stages[lives])
         if lives == 0:
             print("You Lost!")
             end_of_game = True
+    print(art.stages[lives])
     if "_" not in display:
         end_of_game = True 
         print("You Win!")
